6f1adf5cea4c06237dbfb9f48659754c54266d95
dgstjohn <dgstjohn@gmail.com>
2021-10-17 23:48:33 -0400

    rewrote index.js from original bad code

--- index.js
+++ index.js
@@ -1,128 +1,106 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const path = require('path');
 const generateMarkdown = require('./generateMarkdown.js');
-const writeFile = require('./generateSite.js');
 
-// Prompt the user
+// get the questions for the user
 
-const promptProject = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your project?',
-            validate: titleInput => {
-                if (titleInput) {
-                    return true;
-                } else {
-                    console.log('You need to enter a project title!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Please give a description of your project:',
-            validate: descriptionInput => {
-                if (descriptionInput) {
-                    return true;
-                } else {
-                    console.log('You need to enter a project description!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'installation',
-            message: 'Are there specific installation instructions for your project?',
-            default: false
-        },
-        {
-            // if so, 
-            type: 'input',
-            name: 'enterInstallation',
-            message: 'Please describe the installation requirements:',
-            validate: enterInstallationInput => {
-                if (enterInstallationInput) {
-                    return true;
-                } else {
-                    console.log('You need to enter installation requirements!');
-                    return false;
-                }
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub user name?',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false;
             }
         }
-    ])
-};
-
-const promptOther = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Please enter any pertinent usage information:'
-        },
-        {
-            type: 'input',
-            name: 'contribution',
-            message: 'Please enter any contribution guidelines for this project:'
-        },
-        {
-            type: 'input',
-            name: 'test',
-            message: 'Please enter any instructions for testing:'
-        }
-    ])
-};
-
-const promptQuestions = (questions) => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'github',
-            message: 'What is your GitHub user name?',
-            validate: githubInput => {
-                if (githubInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your GitHub username!');
-                    return false;
-                }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address:',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!');
+                return false;
             }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Please enter your email address:',
-            validate: emailInput => {
-                if (emailInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your email address!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'What license will your project use?',
-            choices: ['ISC', 'MIT', 'GNU General Public License', 'Apache', 'BSD', 'Other'] //if 'Other', provide prompt for that 
         }
-    ])
-};
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('You need to enter a project title!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please give a description of your project:',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You need to enter a project description!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'installation',
+        message: 'How are your packages installed for your project?',
+        default: 'npm install'
+    },
+    {
+        // if so, 
+        type: 'input',
+        name: 'enterInstallation',
+        message: 'What command is run to test your project?',
+        default: 'npm test'
+            
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What license will your project use?',
+        choices: ['ISC', 'MIT', 'GNU General Public License', 'Apache', 'BSD', 'None'] 
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter any pertinent usage information:'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please enter any contribution guidelines for this project:'
+    }
+]
 
-promptProject()
-.then(promptOther)
-.then(promptQuestions)
-.then(generateMarkdown)
-.then(markdown => {
-    return writeFile(markdown)
-})
-.then(writeFileResponse => {
-    console.log(writeFileResponse);
-})
-.catch(err => { // standard err catch at end
-    console.log(err);
-  });
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then(inquirerResponses => {
+        console.log('Generating README...');
+        writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
+      });
+}
+
+// Function call to initialize app
+init();
